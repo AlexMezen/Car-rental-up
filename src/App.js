7d6d9e5f7825bdf4626a1de5e520e3680cfa44c9
mez <mezentsev20001028@gmail.com>
2022-01-15 13:01:56 +0200

    Commit1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './Welcome/Header.css';
+import Welcomeheader from './Welcome/Welcomeheader'
+import Welcome from './Welcome/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+        <Welcomeheader/>
+        <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome/Welcomeheader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Header.css';
+function Welcomeheader(){
+    
+    return(
+        <div className = "header">
+            <div className="wrapper">
+             <div className = "left">
+            <a src ="#ent a car">Rent a car</a>
+             </div>
+             <div className = "right">
+            <a src ="#about">About</a>
+            <a src ="#terms">Terms</a>
+             </div> 
+             </div> 
+        </div>
+    );
+    
+} 
+export default Welcomeheader;
