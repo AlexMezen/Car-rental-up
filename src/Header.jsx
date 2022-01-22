a38adc490e6dae86fef22fa81bd8fd68aa1fbb44
mez <mezentsev20001028@gmail.com>
2022-01-22 16:05:46 +0200

    finalized

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
-function Welcomeheader(){
-    
-    return(
-        <div className = "header">
-            <div className="wrapper">
-             <div className = "left">
-            <a href ="#Rent a car">Rent a car</a>
-             </div>
-             <div className = "right">
-            <a href ="#about">About</a>
-            <a href ="#terms">Terms</a>
-             </div> 
-             </div> 
+function Header() {
+  return (
+    <div className="header">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#Rent a car">Rent a car</a>
         </div>
-    );
-    
-} 
+        <div className="right">
+          <a href="#about">About</a>
+          <a href="#terms">Terms</a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default Welcomeheader;
+export default Header;
--- src/Welcome/CarCard.jsx
+++ src/Welcome/CarCard.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './CarCard.css';
+import React from "react";
+import "./CarCard.css";
 
-function CarCard(props){
-   const car = props.car;
-    return(
-    
-         <div className ="carsf">
-            <div className ="carwindow">
-                <div className ="carname">{ car.brand } { car.model }</div>
-                <div className ="carSpecifications">{ car.gearbox } { car.engine }</div>
-                <div className ="pricename">
-                    <div className ="price">{car.pricePerDay}</div>
-                    <div className ="pricedays">
-                       <div className="roundPricedays">
-                        {Math.floor(car.pricePerDay * 7)}
-                        </div>
-                        </div>
-                </div>
-            </div>
+function CarCard({ car }) {
+  return (
+    <div className="carsf">
+      <div className="carwindow">
+        <div className="carname">
+          {car.brand} {car.model}
         </div>
-    
-    )
+        <div className="carSpecifications">
+          {car.gearbox} {car.engine}
+        </div>
+        <div className="pricename">
+          <div className="price">{Math.floor(car.pricePerDay)}</div>
+          <div className="pricedays">
+            <div className="roundPricedays">
+              {Math.floor(car.pricePerDay * 7)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CarCard;
